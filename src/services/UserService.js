4d0f3174df179ef96b256d8359e150d17f327b42
BrVenturaU <brandonventura16@gmail.com>
2021-07-08 01:45:24 -0600

    Crea servicio de usuario

---
+++ src/services/UserService.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import { localStorageManager } from '../utils/LocalStorageManager';
+
+class UserService{
+    #userClient;
+    #sessionTokenKeyName;
+    constructor(){
+        this.path = '/user';
+        this.#sessionTokenKeyName = 'session_token';
+        this.#userClient = this.#configureAxios();
+        this.#setRequestInterceptors();
+    }
+
+    #configureAxios(){
+        return axios.create({
+            baseURL: process.env.VUE_APP_API_URL
+        });
+    }
+
+    // eslint-disable-next-line no-dupe-class-members
+    #setRequestInterceptors(){
+        this.#userClient.interceptors.request.use((request) => {
+            request.headers['Authorization'] = `Bearer ${localStorageManager.getFromLocalStorage(`${this.#sessionTokenKeyName}`)}`;
+            return request;
+        }, (error) => {
+            return Promise.reject(error);
+        });
+    }
+
+    profile(){
+        return this.#userClient.get(`${this.path}/profile`);
+    }
+}
+
+export const userService = new UserService();
+export default{
+    UserService
+}
