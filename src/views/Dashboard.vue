ca58c3cf792bbeffb13b4488085e26ebfe0625dd
BrVenturaU <brandonventura16@gmail.com>
2021-07-08 13:44:58 -0600

    Agrega enrutamiento para home y user

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+//import GoogleMap from '@/components/GoogleMap.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    //GoogleMap
+  }
+}
+</script>
