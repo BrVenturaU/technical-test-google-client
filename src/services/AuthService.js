e9882a895cddc8483056427312edc49242ba00e1
BrVenturaU <brandonventura16@gmail.com>
2021-07-08 23:11:56 -0600

    Agrega cliente base del API para interceptor de respuestas

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,25 +1,18 @@
-import axios from 'axios'
+import { apiClient } from '../utils/BaseApiClient';
 import { localStorageManager } from '../utils/LocalStorageManager';
 
 class AuthService{
-    #authClient;
     #sessionTokenKeyName;
     constructor(){
         this.path = '/authentication';
         this.#sessionTokenKeyName = 'session_token';
-        this.#authClient = this.#configureAxios();
         this.#setRequestInterceptors();
     }
 
-    #configureAxios(){
-        return axios.create({
-            baseURL: process.env.VUE_APP_API_URL
-        });
-    }
 
     // eslint-disable-next-line no-dupe-class-members
     #setRequestInterceptors(){
-        this.#authClient.interceptors.request.use((request) => {
+        apiClient.interceptors.request.use((request) => {
             if(request.url.includes('login') || request.url.includes('register'))
                 return request;
             request.headers['Authorization'] = `Bearer ${localStorageManager.getFromLocalStorage(`${this.#sessionTokenKeyName}`)}`;
@@ -30,15 +23,15 @@ class AuthService{
     }
 
     login(payload){
-        return this.#authClient.post(`${this.path}/login`, payload);
+        return apiClient.post(`${this.path}/login`, payload);
     }
 
     register(payload){
-        return this.#authClient.post(`${this.path}/register`, payload);
+        return apiClient.post(`${this.path}/register`, payload);
     }
 
     refreshToken(){
-        return this.#authClient.get(`${this.path}/refresh`);
+        return apiClient.get(`${this.path}/refresh`);
     }
 
     logout(){
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,25 +1,16 @@
-import axios from 'axios'
+import { apiClient } from '../utils/BaseApiClient';
 import { localStorageManager } from '../utils/LocalStorageManager';
 
 class UserService{
-    #userClient;
     #sessionTokenKeyName;
     constructor(){
         this.path = '/user';
         this.#sessionTokenKeyName = 'session_token';
-        this.#userClient = this.#configureAxios();
         this.#setRequestInterceptors();
     }
-
-    #configureAxios(){
-        return axios.create({
-            baseURL: process.env.VUE_APP_API_URL
-        });
-    }
-
     // eslint-disable-next-line no-dupe-class-members
     #setRequestInterceptors(){
-        this.#userClient.interceptors.request.use((request) => {
+        apiClient.interceptors.request.use((request) => {
             request.headers['Authorization'] = `Bearer ${localStorageManager.getFromLocalStorage(`${this.#sessionTokenKeyName}`)}`;
             return request;
         }, (error) => {
@@ -28,11 +19,11 @@ class UserService{
     }
 
     profile(){
-        return this.#userClient.get(`${this.path}/profile`);
+        return apiClient.get(`${this.path}/profile`);
     }
 
     update(payload){
-        return this.#userClient.put(`${this.path}/profile`, payload);
+        return apiClient.put(`${this.path}/profile`, payload);
     }
 }
 
